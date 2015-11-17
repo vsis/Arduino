a22f392f18bc78ab3dab2363f7b3adadaced6085
Roddy González <roddy.gonzalez.89@gmail.com>
2015-11-17 01:19:01 -0300

    Now I calculate acceleration vector. It needs to be calibrated a little more.

--- Acelerometer/acelerometer/acelerometer.ino
+++ Acelerometer/acelerometer/acelerometer.ino
@@ -1,7 +1,10 @@
 const int X = 0;
 const int Y = 1;
 const int Z = 2;
-
+const float G = 512;
+const float scale_x = 95.97;
+const float scale_y = 106.72;
+const float scale_z = 137.082;
 
 void setup() {
   Serial.begin(9600);
@@ -13,12 +16,16 @@ void setup() {
 
 void loop() {
   int x,y,z;
+  float rx, ry, rz;
+  double current_a;
   x = analogRead(X);
-  delay(10);
   y = analogRead(Y);
-  delay(10);
   z = analogRead(Z);
   delay(10);
-  Serial.println( String(String(x) + ", " + String(y) + ", " + String(z)));
-  delay(1000);
+  rx = ((float)x - G) / scale_x;
+  ry = ((float)y - G) / scale_y;
+  rz = ((float)z - G) / scale_z;
+  current_a = sqrt(sq(rx) + sq(ry) + sq(rz));
+  Serial.println( String(String(rx) + ", " + String(ry) + ", " + String(rz) + ", ") + String(current_a));
+  delay(100);
 }
